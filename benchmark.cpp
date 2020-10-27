317f080cdf5649f76a010f62782968ce0b195e93
Manu-Tran <emmanuel.tran@gmail.com>
2020-10-27 17:53:11 +0100

    ENH: Benchmark using google benchmark framework

---
+++ benchmark.cpp
@@ -0,0 +1,32 @@
+#include "nilmu.hpp"
+#include <vector>
+#include <array>
+#include <chrono>
+#include <benchmark/benchmark.h>
+#include <cstdio>
+#include <iostream>
+
+
+static void BM_nilmu(benchmark::State& state) {
+    int n = 10000;
+    auto vec = std::vector<int>(n);
+    auto nil = nilmu::nilmu(vec, "Done !");
+    nilmu::nil_options.frequency<10>()
+                      .term_width(80);
+    for (auto _ : state) {
+        for (auto itr = nil.begin(), end = nil.end(); itr != end; ++itr){}
+    }
+}
+
+static void BM_classic(benchmark::State& state) {
+    int n = 10000;
+    auto vec = std::vector<int>(n);
+    for (auto _ : state) {
+        for (auto itr(vec.begin()), end = vec.end(); itr != end; itr++){}
+    }
+}
+
+BENCHMARK(BM_nilmu);
+BENCHMARK(BM_classic);
+
+BENCHMARK_MAIN();
